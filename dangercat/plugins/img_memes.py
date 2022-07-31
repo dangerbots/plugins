6c0c4a981375f1584b7cd8dc26b67b68f4959c72
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2022-07-31 13:15:45 +0530

    Update img_memes.py

--- dangercat/plugins/img_memes.py
+++ dangercat/plugins/img_memes.py
@@ -22,8 +22,8 @@ async def _(event):
     await event.get_reply_message()
     img = await convert_to_image(event, event.client)
     imagePath = img
-    maskPath = "./warbot/resources/pics/mask (1).png"
-    cascPath = "./warbot/resources/xmls/haarcascade_frontalface_default.xml"
+    maskPath = "./dangercat/resources/pics/mask (1).png"
+    cascPath = "./dangercat/resources/xmls/haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(cascPath)
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
