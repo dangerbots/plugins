ab869b3e942402091652ec1c97f0baf8e3086d4b
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-07-31 08:23:12 +0530

    Update war_string.py

--- war_string.py
+++ war_string.py
@@ -6,8 +6,8 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-print("•••   WARBOT  SESSION  GENERATOR   •••")
-print("\nHello!! Welcome to WARBot Session Generator\n")
+print("•••   Dangercat  SESSION  GENERATOR   •••")
+print("\nHello!! Welcome to dangercat Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
     print("Choose the string session type: \n1. WarBot \n2. Music Bot")
@@ -18,14 +18,14 @@ if okvai == "69":
         API_HASH = input("\nEnter API HASH here: ")
         with TelegramClient(StringSession(), APP_ID, API_HASH) as warbot:
             print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{warbot.session.save()}`")
+            warbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{dangercat.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as warbot:
             print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#WARBOT_MUSIC #WARBOT_SESSION\n\n`{warbot.export_session_string()}`")
+            warbot.send_message("me", f"#dangercat_MUSIC #WARBOT_SESSION\n\n`{warbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
