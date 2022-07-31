f83e089f7febf1c25be2bad1406ab4b88d1ff9a7
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2022-07-31 13:16:44 +0530

    Update mmf.py

--- dangercat/helpers/mmf.py
+++ dangercat/helpers/mmf.py
@@ -14,7 +14,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "warbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "dangercat/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
@@ -121,7 +121,7 @@ async def draw_meme(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "warbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "dangercat/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
