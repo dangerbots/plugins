7227520e6d03605c325b36c9155ec6b8ec02d105
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2022-07-31 15:25:13 +0530

    Update dangercat_string.py

--- dangercat_string.py
+++ dangercat_string.py
@@ -23,7 +23,7 @@ if okvai == "69":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as warbot:
+        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as dangercat:
             print("\nYour dangercat Session Is sent in your Telegram Saved Messages.")
             dangercat.send_message("me", f"#dangercat_MUSIC #DANGERCAT_SESSION\n\n`{dangercat.export_session_string()}`")
     else:
