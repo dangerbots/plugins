054bb62c5cb4389ee0c3578d79a22b77d8e6aa46
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-07-31 08:25:21 +0530

    Update war_string.py

--- war_string.py
+++ war_string.py
@@ -16,16 +16,16 @@ if okvai == "69":
         print("\nTelethon Session For WarBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with TelegramClient(StringSession(), APP_ID, API_HASH) as warbot:
+        with TelegramClient(StringSession(), APP_ID, API_HASH) as dangercat:
             print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{dangercat.session.save()}`")
+            dangercat.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{dangercat.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as warbot:
             print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            warbot.send_message("me", f"#dangercat_MUSIC #WARBOT_SESSION\n\n`{warbot.export_session_string()}`")
+            dangercat.send_message("me", f"#dangercat_MUSIC #WARBOT_SESSION\n\n`{warbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
