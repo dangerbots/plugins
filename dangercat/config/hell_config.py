133844f0ca9eb66440274af492aaef3f965609a9
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-08-03 09:44:00 +0530

    Update hell_config.py

--- dangercat/config/hell_config.py
+++ dangercat/config/hell_config.py
@@ -43,8 +43,8 @@ class Config(object):
         LOGGER_ID = int(LOGGER_ID)
     MAX_MESSAGE_SIZE_LIMIT = 4095
     MAX_SPAM = int(os.environ.get("MAX_SPAM", 7))
-    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "waruserbot")
-    MY_GROUP = os.environ.get("YOUR_GROUP", "waruserbotsupport")
+    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "danger_bots")
+    MY_GROUP = os.environ.get("YOUR_GROUP", "dangerbots")
     OCR_API = os.environ.get("OCR_API", None)
     PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL", None)
     if PLUGIN_CHANNEL:
