b310f93cd27581629e1d96f67aa0abada03ba77c
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-08-03 23:35:36 +0530

    Update _core.py

--- dangercat/plugins/_core.py
+++ dangercat/plugins/_core.py
@@ -27,13 +27,13 @@ async def kk(event):
     _o = o.split("\n")
     o = "\n".join(_o)
     OUTPUT = f"""
-<h1>List of Plugins in Waruserbot:</h1>
+<h1>List of Plugins in Dangercat:</h1>
 
 <code>{o}</code>
 
 <b><i>HELP:</b></i> <i>If you want to know the commands for a plugin, do “ .plinfo <plugin name> ”
 
-<b><a href='https://t.me/waruserbot'>@waruserbot</a></b>
+<b><a href='https://t.me/danger_bots'>@dangerbots</a></b>
 """
     hell = await telegraph_paste("All available plugins in waruserbot", OUTPUT)
     await eor(event, f"[All available plugins in waruserbot]({hell})", link_preview=False)
@@ -46,7 +46,7 @@ async def send(event):
     message_id = event.reply_to_msg_id or event.message.id
     thumb = hell_logo
     input_str = event.pattern_match.group(1)
-    omk = f"**• Plugin name ≈** `{input_str}`\n**• Uploaded by ≈** {hell_mention}\n\n⚡ **[⚡️ʟᴇɢᴇɴᴅᴀʀʏ ᴀғ ᴡᴀʀᴜsᴇʀʙᴏᴛ⚡️]({chnl_link})** ⚡"
+    omk = f"**• Plugin name ≈** `{input_str}`\n**• Uploaded by ≈** {hell_mention}\n\n⚡ **[⚡️DANGERCAT⚡️]({chnl_link})** ⚡"
     the_plugin_file = "./dangercat/plugins/{}.py".format(input_str.lower())
     if os.path.exists(the_plugin_file):
         await event.client.send_file(
