a724f9a1878f80a9e3ff95fa9fe0982bcb45e686
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-08-03 23:46:10 +0530

    Update __main__.py

--- dangercat/__main__.py
+++ dangercat/__main__.py
@@ -14,7 +14,7 @@ from dangercat.version import __hell__ as hellver
 
 hl = Config.HANDLER
 
-DCAT_PIC = "https://telegra.ph/file/a711979a5c26d791209fa.jpg"
+DCAT_PIC = "https://telegra.ph/file/ae342404fdf956686b8f5.jpg"
 
 
 # Client Starter
