97f563e1099575762c27995970f6e83af7b264cc
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-08-14 16:02:22 +0530

    Update image.py

--- dangercat/helpers/image.py
+++ dangercat/helpers/image.py
@@ -22,29 +22,7 @@ async def invert_colors(imagefile, endname):
 async def flip_image(imagefile, endname):
     image = Image.open(imagefile)
     inverted_image = PIL.ImageOps.flip(image)
-    inverted_image.save(endname)
-
-
-# gray tone...
-async def grayscale(imagefile, endname):
-    image = Image.open(imagefile)
-    inverted_image = PIL.ImageOps.grayscale(image)
-    inverted_image.save(endname)
-
-
-# mirrored...
-async def mirror_file(imagefile, endname):
-    image = Image.open(imagefile)
-    inverted_image = PIL.ImageOps.mirror(image)
-    inverted_image.save(endname)
-
-
-# sun kissed... 
-# print("Agar Suraj ne sachme chum lia to gand fatt jaegi")
-async def solarize(imagefile, endname):
-    image = Image.open(imagefile)
-    inverted_image = PIL.ImageOps.solarize(image, threshold=128)
-    inverted_image.save(endname)
+    inverted_imag
 
 
 # dangercat
